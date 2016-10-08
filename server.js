8a1bc9a1c5a774107e0bbd8b961e7d6401514fce
Tanay Vardhan <tvardha2@illinois.edu>
2016-10-08 09:50:18 +0000

    I learnt how to route. Sick one

--- server.js
+++ server.js
@@ -3,5 +3,16 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
+
+//RouterBoy
+app.post('/', function (req, res) {
+  res.send('POST request to the homepage');
+});
+
+app.get('/', function (req, res) {
+  res.send('Starboy');
+});
+
+
 app.listen(port);
 console.log('Server started! At http://localhost:' + port);
