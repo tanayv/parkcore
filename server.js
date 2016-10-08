17864f56109229423c093204b20d0a2a404a40c6
Tanay Vardhan <tvardha2@illinois.edu>
2016-10-08 10:16:27 +0000

    Hooked it with MySQL. Too sick

--- server.js
+++ server.js
@@ -13,6 +13,30 @@ app.get('/', function (req, res) {
   res.send('Starboy');
 });
 
+//SeequlBoy
+var mysql = require("mysql");
+
+// First you need to create a connection to the db
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "tanayv",
+  password: ""
+});
+
+con.connect(function(err){
+  if(err){
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
+
+con.end(function(err) {
+  // The connection is terminated gracefully
+  // Ensures all previously enqueued queries are still
+  // before sending a COM_QUIT packet to the MySQL server.
+});
+
 
 app.listen(port);
 console.log('Server started! At http://localhost:' + port);
